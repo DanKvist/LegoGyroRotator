f761464082481fcee04446016d5bc6a8e6aa46fe
Dan Thilderkvist <dan.thilderkvist@gmail.com>
2017-03-13 21:20:44 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from ev3dev.ev3 import *
+
+gy = GyroSensor()
+
+#Put sensor in ANGLE mode
+gy.mode('GYRO-ANG')
+
+units = gy.units
+
+while true
+	angle = gy.value()
+	print(str(angle) + " " + units)
+
+
