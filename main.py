45a7aeaa8a734eeb226dbfd3552ec5d410074900
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2017-03-16 17:22:22 +0000

    added US sensor and display to screen

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ from time import sleep
 import ev3dev.fonts as fonts
 
 gy = GyroSensor()
+us = UltrasonicSensor()
 lcd = Screen()
 dispFont = fonts.load('luBS24')
 
 
 #Put sensor in ANGLE mode
 gy.mode='GYRO-ANG'
+us.mode='US-DIST-CM'
 
 units = gy.units
 
 while True:
 	lcd.clear()
 	angle = gy.value()
+	distance = us.value()/10
 	lcd.draw.text((30,60), str(angle) + " " + units, font=dispFont)
+	lcd.draw.text((30,30), str(distance) + " cm", font=dispFont)
 	lcd.update()
 	sleep(0.5)
 
