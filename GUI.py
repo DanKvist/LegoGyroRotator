ef0ba350dd4795b1c3d33b7ba2b724b8e2b44aa0
Dan Thilderkvist <dan.thilderkvist@gmail.com>
2017-03-19 20:57:07 +0100

    fixed GUI to run on python3

--- GUI.py
+++ GUI.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python
 
-import TKinter as tk
+from tkinter import *
+import time
+import math
+
+root=Tk()
+root.title="Radar"
+
+windowFrameThickness = 10
+
+windowHeight = 800 + windowFrameThickness*2
+windowWidth = windowHeight
+radarRadius = 400
+
+centre = [windowHeight/2, windowWidth/2]
+
+canvas = Canvas(root,
+	      width=windowWidth,
+	      height=windowHeight)
+
+canvas.pack()
+
+canvas.create_oval(centre[0] - radarRadius,
+		   centre[1] - radarRadius,
+		   centre[0] + radarRadius,
+		   centre[1] + radarRadius,
+		   width = 5)
+
+iterator = 0
+while True:
+	root.update_idletasks()
+	root.update()
+
+	x = centre[0] + radarRadius*math.cos(iterator)
+	y = centre[1] + radarRadius*math.sin(iterator)
+
+	canvas.create_line(centre[0], centre[1], x, y, width = 2)
+	iterator = iterator + 1
+
+	time.sleep(0.01)
 
 
-app = radarWindow(None)
-app.title('SYSTEM CONTROL')
-
-
-class radarWindow(Tkinter.Tk):
-	def __init__(self,parent):
-		TKinter.Tk.__init__(self,parent)
-		self.parent = parent
-		self.initialize()
-
-	def initialize(self):
-		pass
